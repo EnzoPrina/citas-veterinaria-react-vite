ad38fba71cd58fb9955dec53d5c8d17eef057b6d
Enzo Prina <enzoprina9@gmail.com>
2022-10-29 10:38:30 -0300

    github

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+const Header = () => {
+    return(
+        <>
+            <div>
+                <p className="bg-indigo-600 mt-10"></p>
+                <h1 className="font-black text-indigo-200 text-5xl text-center md:w-2/3 mx-auto pt-10">
+                    Seguimiento Pacientes {''}
+                    <span className="text-white rounded-md">  Veterinaria </span>
+                    
+                </h1>
+            </div>
+        </>
+    )   
+}
+
+export default Header;
